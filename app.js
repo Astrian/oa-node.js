6a3fbe3dc1568dabe971c200ea01b509d723e99e
Astrian Zheng <mingriqd@gmail.com>
2017-04-29 10:59:47 +0800

    引入前端

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -34,6 +34,8 @@ app.use(session({
 
 app.use('/', index);
 app.use('/api', api);
+app.use('/resource', express.static('node_modules'));
+app.use('/public', express.static('public'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
