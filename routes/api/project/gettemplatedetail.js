f472aa8117fed1952add7d01c99824710e5ae789
Astrian Zheng <mingriqd@gmail.com>
2017-05-21 08:56:49 +0800

    重构获取模板详情、获取模板列表两个接口的代码

--- routes/api/project/gettemplatedetail.js
+++ routes/api/project/gettemplatedetail.js
@@ -1,17 +1,15 @@
 var debug = require('debug')('oa: api/project/getprojectdetail');
-var 调用数据库 = require('../../modules/Db').exec
-var 回调函数是一个反人类的东西 = require('sync_back').run
-var 空 = null
-module.exports = function (req, res, api, 请求体) {回调函数是一个反人类的东西(function* (回调) {
-  var 成功返回 = api.back4Success
-  var 失败返回 = api.back4Fail
-  if(!请求体.id || 请求体.id =='') return 失败返回(400,0,'未填写模板 ID。')
-  var 登录用户 = req.session.user
-  var SQL语句 = 'SELECT * FROM project_temple WHERE (状态 = 1 OR 创建者 = '+登录用户+') AND id = '+请求体.id
-  var 返回 = yield 调用数据库(SQL语句, 回调.next)
-  if(!返回[0]) return 失败返回(404,0,'专案不存在，或不是当前用户创建。')
-  返回 = 返回[0]
-  返回.表字段 = JSON.parse(返回.表字段)
-  返回.流程 = JSON.parse(返回.流程)
-  成功返回(返回)
+var dbOps = require('../../modules/Db').exec
+var cleanCallback = require('sync_back').run
+module.exports = function (req, res, api, reqBody) {cleanCallback(function* (callback) {
+  var return4Success = api.back4Success
+  var return4Fail = api.back4Fail
+  if(!reqBody.id || reqBody.id =='') return return4Fail(400,0,'未填写模板 ID。')
+  var loginUID = req.session.user
+  var SQLStatement = 'SELECT * FROM project_temple WHERE (status = 1 OR creator = '+loginUID+') AND id = '+reqBody.id
+  var result = yield dbOps(SQLStatement, callback.next)
+  if(!result[0]) return return4Fail(404,0,'专案不存在，或不是当前用户创建。')
+  result = result[0]
+  result.sheets = JSON.parse(result.sheets)
+  return4Success(result)
 })}
--- routes/api/project/gettemplatelist.js
+++ routes/api/project/gettemplatelist.js
@@ -1,25 +1,25 @@
-var 调用数据库 = require('../../modules/Db').exec
+var dbOps = require('../../modules/Db').exec
 var debug = require('debug')('oa: api/project/gettemplatelist');
-var 回调函数是一个反人类的东西 = require('sync_back').run
+var cleanCallback = require('sync_back').run
 var 空 = null
-module.exports = function (req, res, api, 请求体) {
-  回调函数是一个反人类的东西(function* (回调) {
-    var 成功返回 = api.back4Success
-    var 失败返回 = api.back4Fail
-    if(!请求体.type || 请求体.type == '') return 失败返回(400,0,'未填写请求列表类型。')
-    var 登录用户 = req.session.user
-    var SQL语句
-    if (请求体.type == 1) {
-      SQL语句 = 'SELECT id, 标题, 描述 FROM project_temple WHERE 状态 = 1';
-      var 结果 = yield 调用数据库(SQL语句, 回调.next);
-      return 成功返回(结果)
-    }else if (请求体.type == 2) {
-      SQL语句 = 'SELECT id, 标题, 描述 FROM project_temple WHERE 创建者 = '+登录用户;
-      debug(SQL语句)
-      var 结果 = yield 调用数据库(SQL语句, 回调.next);
-      return 成功返回(结果)
+module.exports = function (req, res, api, reqBody) {
+  cleanCallback(function* (callback) {
+    var return4Success = api.back4Success
+    var return4Fail = api.back4Fail
+    if(!reqBody.type || reqBody.type == '') return return4Fail(400,0,'未填写请求列表类型。')
+    var loginUID = req.session.user
+    var SQLStatement
+    if (reqBody.type == 1) {
+      SQLStatement = 'SELECT id, title, description FROM project_temple WHERE status = 1';
+      var dbResult = yield dbOps(SQLStatement, callback.next);
+      return return4Success(dbResult)
+    }else if (reqBody.type == 2) {
+      SQLStatement = 'SELECT id, title, description FROM project_temple WHERE creator = '+loginUID
+      debug(SQLStatement)
+      var dbResult = yield dbOps(SQLStatement, callback.next);
+      return return4Success(dbResult)
     }else{
-      return 失败返回(400,1,'请求类型不正确。')
+      return return4Fail(400,1,'请求类型不正确。')
     }
   })
 }
