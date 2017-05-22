b5cb8b0b01d8206bb40fff2286307cf4453ac9cb
Astrian Zheng <mingriqd@gmail.com>
2017-05-22 17:29:04 +0800

    新建流程可以新增带部门经理、自主专案流转的流程了。同时重写了新专案接口。

--- routes/api/project/newflow.js
+++ routes/api/project/newflow.js
@@ -14,10 +14,16 @@ module.exports = function (req, res, api, reqBody) {cleanCallback(function* (cal
   if(!reqBody.description || reqBody.description == '') return return4Fail(400,0, "描述未填写。")
   if(!reqBody.flow || reqBody.flow == '') return return4Fail(400,0, "流程未填写。")
   for (var i in reqBody.flow){
-    SQLStatement = 'SELECT id FROM user WHERE id = '+reqBody.flow[i]
-    var result = (yield dbOps(SQLStatement, callback.next))[0]
-    var j = (+i)+1
-    if(!result) return return4Fail(400, 1, '流程中，第 '+j+' 步 UID 不存在。')
+    if(reqBody.flow[i] == -1){
+      if(i != 0) return return4Fail(400,3,'非第一步无法选择「申请人的部门经理」的选项.')
+    }else if(reqBody.flow[i] == -2){
+      if(i == 0) return return4Fail(400,2,'第一步无法选择「由上一步决定此步骤负责人」的选项。')
+    }else{
+      SQLStatement = 'SELECT id FROM user WHERE id = '+reqBody.flow[i]
+      var result = (yield dbOps(SQLStatement, callback.next))[0]
+      var j = (+i)+1
+      if(!result) return return4Fail(400, 1, '流程中，第 '+j+' 步 UID 不存在。')
+    }
   }
   SQLStatement = "INSERT INTO flow (title, description, flow, status) VALUES('"+reqBody.title+"', '"+reqBody.description+"', '"+JSON.stringify(reqBody.flow)+"', 1)"
   var result = yield dbOps(SQLStatement, callback.next)
--- routes/api/project/newproject.js
+++ routes/api/project/newproject.js
@@ -1,39 +1,36 @@
 var debug = require('debug')('oa: api/project/newtemplate');
-var 调用数据库 = require('../../modules/Db').exec
-var 回调函数是一个反人类的东西 = require('sync_back').run
-var 类型判断 = require('util')
+var dbOps = require('../../modules/Db').exec
+var cleanCallback = require('sync_back').run
+var typeJudger = require('util')
 var 空 = null
 
-module.exports = function (req, res, api, 请求体) {
-  回调函数是一个反人类的东西 (function *(回调){
-    var 失败返回 = api.back4Fail
-    var 成功返回 = api.back4Success
-    var 登录用户 = req.session.user
-    if(!请求体.模板ID || 请求体.模板ID == '') return 失败返回(400, 3, '未填写模板 ID。')
-    if(!请求体.数据 || 请求体.数据 == '') return 失败返回(400, 3, '未填写数据。')
-    var SQL语句 = 'SELECT * FROM project_temple WHERE id = '+请求体.模板ID
-    var 数据库结果 = yield 调用数据库(SQL语句, 回调.next)
-    if(!数据库结果[0] || 数据库结果[0].状态 != 1) return 失败返回(404,1,'模板不存在（ID 错误），或尚未发布。')
-    var 专案模板数据 = JSON.parse(数据库结果[0].表字段)
-    var 用户填写数据 = 请求体.数据
-    if(专案模板数据.length != 用户填写数据.length) return 失败返回(400, 0, '字段不完整。')
-    for(var i in 专案模板数据){
-      if(!类型判断.isArray(专案模板数据[i].类型)){
-        switch (专案模板数据[i].类型){
-          case '字段': if(!(类型判断.isString(用户填写数据[i]))) return 失败返回(400, 0, '字段 '+专案模板数据[i].名称+' 的类型出错，它应该是一个字段。'); break;
-          case '文本': if(!(类型判断.isString(用户填写数据[i]))) return 失败返回(400, 0, '字段 '+专案模板数据[i].名称+' 的类型出错，它应该是一段文本。'); break;
-          case '数字': if(!(类型判断.isNumber(+用户填写数据[i]))) return 失败返回(400, 0, '字段 '+专案模板数据[i].名称+' 的类型出错，它应该是一个数字。'); break;
-        }
-      }
-      else{
-        if(!(类型判断.isNumber(用户填写数据[i]))) return 失败返回(400, 0, '字段 '+专案模板数据[i].名称+' 的类型出错，它应该是相应数组中某一个元素的位址。')
-        if(!专案模板数据[i].类型[用户填写数据[i]]) return 失败返回(400, 0, '所选数据（'+用户填写数据[i]+'）不在数组 '+专案模板数据[i].名称+' 的范围内')
-      }
+module.exports = function (req, res, api, reqBody) {cleanCallback (function *(callback){
+  var return4Fail = api.back4Fail
+  var return4Success = api.back4Success
+  var loginUID = req.session.user
+  if(!reqBody.template || reqBody.template == '') return return4Fail(400, 3, '未填写模板 ID。')
+  if(!reqBody.data || reqBody.data == '') return return4Fail(400, 3, '无数据。')
+  var SQLStatement = 'SELECT * FROM project_temple WHERE status = 1 AND id = '+reqBody.template
+  var result = yield dbOps(SQLStatement, callback.next)
+  if(!result[0]) return return4Fail(404,1,'模板不存在（ID 错误），或尚未发布。')
+  var forms = JSON.parse(result[0].sheets)
+  var data = reqBody.data
+  if(forms.length != data.length) return return4Fail(400, 0, '字段不完整。')
+  for(var i in forms){
+    switch(forms[i].type){
+      case 'string':
+        if(!typeJudger.isString(data[i])) return4Fail(400,1,'第 '+((+i)+1)+ ' 位数据应该是一个字符串，但它是一个数字。')
+        break
+      case 'text':
+        if(!typeJudger.isString(data[i])) return4Fail(400,1,'第 '+((+i)+1)+ ' 位数据应该是一个文本，但它是一个数字。')
+        break
+      case 'number':
+        if(!typeJudger.isNumber(data[i])) return4Fail(400,1,'第 '+((+i)+1)+ ' 位数据应该是一个文本，但它是一个数字。')
     }
-    SQL语句 = "INSERT INTO project (申请人, 申请时间, 模板, 数据, 状态) VALUES ('" + 登录用户 + "', " + new Date().getTime() + ",'"+请求体.模板ID+"', '"+JSON.stringify(用户填写数据)+"', 0)";
-    数据库结果 = yield 调用数据库(SQL语句, 回调.next)
-    成功返回({
-      专案ID: 数据库结果.insertId
-    })
+  }
+  SQLStatement = "INSERT INTO project (applyer, submittime, template, data, status) VALUES ('"+loginUID+"', '"+new Date().getTime()+"','"+reqBody.template+"','"+JSON.stringify(reqBody.data)+"', -1)"
+  result = yield dbOps(SQLStatement, callback.next)
+  return4Success({
+    id: result.insertId
   })
-}
+})}
