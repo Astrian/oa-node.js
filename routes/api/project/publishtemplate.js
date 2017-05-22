cc218f8c0b1934a51e1698b813b1e10f7d254c20
Astrian Zheng <mingriqd@gmail.com>
2017-05-22 12:41:15 +0800

    发布模板接口更新

--- routes/api/project/publishtemplate.js
+++ routes/api/project/publishtemplate.js
@@ -1,19 +1,23 @@
 var debug = require('debug')('oa:api/project/publishtemplate')
-var 调用数据库 = require('../../modules/Db').exec
-var 回调函数是一个反人类的东西 = require('sync_back').run
-module.exports = function (req, res, api, 请求体) {
-  回调函数是一个反人类的东西(function* (回调) {
-    var 当前登录用户 = req.session.user
-    var 失败返回 = api.back4Fail
-    var 成功返回 = api.back4Success
-    if (!请求体.模板ID || 请求体.模板ID == '') return 失败返回(400, 0, "模板 ID 未填写。")
-    var SQL语句 = 'SELECT 创建者,状态 FROM project_temple WHERE id = '+请求体.模板ID
-    var 调用结果 = yield 调用数据库(SQL语句, 回调.next)
-    if(!调用结果[0]) return 失败返回(404, 0, '模板不存在')
-    if(调用结果[0].状态 != 0) return 失败返回(400, 1, '目标模板不在草稿状态。')
-    if(当前登录用户 != 调用结果[0].创建者) return 失败返回(401, 2, '当前用户不是目标模板的创建者。')
-    SQL语句 = "UPDATE project_temple SET 状态 = 1 WHERE id = "+请求体.模板ID
-    yield 调用数据库(SQL语句, 回调.next)
-    成功返回(null)
-  })
-}
+var dbOps = require('../../modules/Db').exec
+var cleanCallback = require('sync_back').run
+module.exports = function (req, res, api, reqBody) {cleanCallback(function* (callback) {
+  var return4Fail = api.back4Fail
+  var return4Success = api.back4Success
+  var loginUID = req.session.user
+  var SQLStatement = 'SELECT status, node FROM user WHERE id = '+loginUID
+  var userInfo = (yield dbOps(SQLStatement, callback.next))[0]
+  SQLStatement = 'SELECT ispersonnel FROM node WHERE id = '+userInfo.node
+  userInfo.isPersonnel = (yield dbOps(SQLStatement, callback.next))[0].ispersonnel
+  if(userInfo.status != 2 && userInfo.isPersonnel != 1) return return4Fail(401, 0, '当前登录用户无权使用本接口。')
+  if(!reqBody.template || reqBody.template == '') return return4Fail(400,0, "未选定模板。")
+  SQLStatement = 'SELECT * FROM project_temple WHERE id = '+reqBody.template
+  var template = (yield dbOps(SQLStatement, callback.next))[0]
+  if(!template) return return4Fail(404,0, "目标模板不存在。")
+  if(template.status != 0) return return4Fail(404,1, "目标模板不在草稿状态。")
+  SQLStatement = 'SELECT * FROM bind WHERE template = '+reqBody.template
+  if(!(yield dbOps(SQLStatement, callback.next))[0]) return return4Fail(400, 2, '模板尚未绑定流程。')
+  SQLStatement = 'UPDATE project_temple SET status = 1 WHERE id = '+reqBody.template
+  yield dbOps(SQLStatement, callback.next)
+  return4Success(null)
+})}
