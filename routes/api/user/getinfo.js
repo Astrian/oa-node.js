b3ebb4810774c676da3af30aaadc4f1c860600e8
Astrian Zheng <mingriqd@gmail.com>
2017-05-23 21:08:56 +0800

    更新获取用户数据接口

--- routes/api/user/getinfo.js
+++ routes/api/user/getinfo.js
@@ -1,18 +1,17 @@
 var debug = require('debug')('oa:api/user/recovery');
-var 调用数据库 = require('../../modules/Db').exec
-var 回调函数是一个反人类的东西 = require('sync_back').run
+var dbOps = require('../../modules/Db').exec
+var cleanCallback = require('sync_back').run
 var 空 = null
 module.exports = function (req, res, api, post) {
   var 请求体 = req.body
   var 登录用户 = req.session.user
-  回调函数是一个反人类的东西(function* (回调) {
-    var 失败返回 = api.back4Fail
-    var 成功返回 = api.back4Success
-    var SQL语句 = 'SELECT id, 用户名, 姓, 名, 头像, 所属部门, 帐户状态 FROM user WHERE id = '+登录用户
-    var 调用结果 = yield 调用数据库(SQL语句, 回调.next)
-    var 用户 = 调用结果[0]
-    var SQL语句 = 'SELECT * FROM node WHERE id = '+用户.所属部门
-    用户.所属部门 = (yield 调用数据库(SQL语句, 回调.next))[0]
-    成功返回(用户)
+  cleanCallback(function* (callback) {
+    var back4Fail = api.back4Fail
+    var back4Success = api.back4Success
+    var SQLStatement = 'SELECT id, username, firstname, lastname, avatar, node, status FROM user WHERE id = '+登录用户
+    var user = (yield dbOps(SQLStatement, callback.next))[0]
+    var SQLStatement = 'SELECT * FROM node WHERE id = '+user.node
+    user.node = (yield dbOps(SQLStatement, callback.next))[0]
+    back4Success(user)
   })
 }
