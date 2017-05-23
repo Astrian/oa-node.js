16b37506715f4ae73e9650da4eccd0031454b3f7
Astrian Zheng <mingriqd@gmail.com>
2017-05-23 17:14:33 +0800

    一个小重构

--- routes/api/user/signout.js
+++ routes/api/user/signout.js
@@ -1,6 +1,5 @@
-var 空 = null
 module.exports = function (req, res, api, post) {
-    var 成功返回 = api.back4Success
+    var return4Success = api.back4Success
     req.session.user = null
-    成功返回(空)
+    return4Success(null)
 }
