8f7fbdf4722f79946959cd20d9a76f647297bc16
Astrian Zheng <mingriqd@gmail.com>
2017-05-23 17:02:36 +0800

    重构帐户恢复接口

--- routes/api/user/confirmtoken.js
+++ routes/api/user/confirmtoken.js
@@ -1,37 +1,36 @@
 var debug = require('debug')('oa:api/user/recovery');
-var 调用数据库 = require('../../modules/Db').exec
-var 动态验证码 = require('authenticator')
-var 回调函数是一个反人类的东西 = require('sync_back').run
-var 空 = null
+var dbOps = require('../../modules/Db').exec
+var authenticator = require('authenticator')
+var cleanCallback = require('sync_back').run
 module.exports = function (req, res, api, post) {
-  var 请求体 = req.body
-  var 失败返回 = api.back4Fail
-  var 成功返回 = api.back4Success
-  回调函数是一个反人类的东西(function* (回调值) {
-    if (!请求体.用户名 || 请求体.用户名 == '') return 失败返回(400, 0, '用户名未填写。')
-    if (!请求体.恢复密钥 || 请求体.恢复密钥 == '') return 失败返回(400, 0, '恢复密钥未填写。')
-    if (!请求体.动态验证码 || 请求体.动态验证码 == '') return 失败返回(400, 0, '动态验证码未填写。')
-    var SQL语句 = 'SELECT * FROM user WHERE 用户名 = "' + 请求体.用户名 + '"'
-    var 数据库结果 = yield 调用数据库(SQL语句, 回调值.next)
-    var 用户
-    if (!数据库结果[0]) return 失败返回(400, 1, '用户名不存在。')
-    if(数据库结果[0].帐户状态 != 0) return 失败返回(400, 2, '帐户不在恢复状态')
-    用户 = 数据库结果[0]
-    SQL语句 = 'SELECT * FROM user_recovery WHERE id = ' + 用户.id + ' AND 密钥 = "' + 请求体.恢复密钥 + '"'
-    var 数据库结果 = yield 调用数据库(SQL语句, 回调值.next)
-    if (!数据库结果[0]) return 失败返回(401, 1, '恢复密钥错误，或帐户不在恢复状态。')
-    var 验证结果 = 动态验证码.verifyToken(用户.密钥, 请求体.动态验证码);
-    if (验证结果) {
-      if (验证结果.delta == 0) {
-        req.session.user = 用户.id
-        SQL语句 = 'DELETE FROM user_recovery WHERE id = ' + 用户.id
-        yield 调用数据库(SQL语句, 回调值.next)
-        SQL语句 = 'UPDATE user SET 帐户状态 = 1 WHERE id = '+用户.id
-        yield 调用数据库(SQL语句, 回调值.next)
-        return 成功返回(空)
+  var reqBody = req.body
+  var return4Fail = api.back4Fail
+  var return4Success = api.back4Success
+  cleanCallback(function* (callback) {
+    if (!reqBody.username || reqBody.username == '') return return4Fail(400, 0, '用户名未填写。')
+    if (!reqBody.recovery || reqBody.recovery == '') return return4Fail(400, 0, '恢复密钥未填写。')
+    if (!reqBody.code || reqBody.code == '') return return4Fail(400, 0, '验证码未填写。')
+    var SQLStatement = 'SELECT * FROM user WHERE username = "' + reqBody.username + '"'
+    var result = yield dbOps(SQLStatement, callback.next)
+    var user
+    if (!result[0]) return return4Fail(400, 1, '用户名不存在。')
+    if(result[0].status != 0) return return4Fail(400, 2, '帐户不在恢复状态')
+    user = result[0]
+    SQLStatement = 'SELECT * FROM user_recovery WHERE id = ' + user.id + ' AND recovery = "' + reqBody.recovery + '"'
+    var result = yield dbOps(SQLStatement, callback.next)
+    if (!result[0]) return return4Fail(401, 1, '恢复密钥错误，或帐户不在恢复状态。')
+    var verifyResult = authenticator.verifyToken(user.token, reqBody.code);
+    if (verifyResult) {
+      if (verifyResult.delta == 0) {
+        req.session.user = user.id
+        SQLStatement = 'DELETE FROM user_recovery WHERE id = ' + user.id
+        yield dbOps(SQLStatement, callback.next)
+        SQLStatement = 'UPDATE user SET status = 1 WHERE id = '+user.id
+        yield dbOps(SQLStatement, callback.next)
+        return return4Success(null)
       }
-      else return 失败返回(500, 0, '未知的服务器错误。')
+      else return return4Fail(500, 0, '未知的服务器错误。')
     }
-    else return 失败返回(400, 2, '动态验证码不正确，或已过期。')
+    else return return4Fail(400, 2, '动态验证码不正确，或已过期。')
   })
 }
--- routes/api/user/recovery.js
+++ routes/api/user/recovery.js
@@ -1,28 +1,25 @@
 var debug = require('debug')('oa:api/user/recovery');
-var 调用数据库 = require('../../modules/Db').exec
-var 动态验证码 = require('authenticator')
-var 回调函数是一个反人类的东西 = require('sync_back').run
-module.exports = function (req, res, api, post) {
-  var 请求体 = req.body
-  var 失败返回 = api.back4Fail
-  var 成功返回 = api.back4Success
-  回调函数是一个反人类的东西(function* (回调值) {
-    if(!请求体.用户名 || 请求体.用户名=='') return 失败返回(400, 0, '用户名未填写。')
-    if(!请求体.恢复密钥 || 请求体.恢复密钥=='') return 失败返回(400, 0, '恢复密钥未填写。')
-    var SQL语句 = 'SELECT * FROM user WHERE 用户名 = "'+请求体.用户名+'"'
-    var 数据库结果 = yield 调用数据库(SQL语句,回调值.next) 
-    var 用户
-    if(!数据库结果[0]) return 失败返回(400, 1, '用户名不存在。')
-    if(数据库结果[0].帐户状态 != 0) return 失败返回(400, 2, '帐户不在恢复状态')
-    else 用户 = 数据库结果[0]
-    SQL语句 = 'SELECT * FROM user_recovery WHERE id = '+用户.id+' AND 密钥 = "'+请求体.恢复密钥+'"'
-    var 数据库结果 = yield 调用数据库(SQL语句,回调值.next)
-    if(!数据库结果[0]) return 失败返回(401, 1, '恢复密钥错误，或帐户不在恢复状态。')
-    var 动态码密钥 = 动态验证码.generateKey()
-    SQL语句 = 'UPDATE user SET 密钥 = "'+动态码密钥+'" WHERE id = '+用户.id
-    yield 调用数据库(SQL语句,回调值.next)
-    return 成功返回({
-      二维码: 动态验证码.generateTotpUri(动态码密钥, (用户.姓).concat(用户.名),'企业 OA 系统', 'SHA1', 6, 30)
-    })
+var dbOps = require('../../modules/Db').exec
+var authenticator = require('authenticator')
+var cleanCallback = require('sync_back').run
+module.exports = function (req, res, api, post) {cleanCallback(function* (callback) {
+  var reqBody = req.body
+  var return4Fail = api.back4Fail
+  var return4Success = api.back4Success
+  if(!reqBody.username || reqBody.username=='') return return4Fail(400, 0, '用户名未填写。')
+  if(!reqBody.recovery || reqBody.recovery=='') return return4Fail(400, 0, '恢复密钥未填写。')
+  var SQLStatement = 'SELECT * FROM user WHERE username = "'+reqBody.username+'"'
+  var user = yield dbOps(SQLStatement,callback.next)
+  if(!user[0]) return return4Fail(400, 1, '用户名不存在。')
+  if(user[0].status != 0) return return4Fail(400, 2, '帐户不在恢复状态')
+  else user = user[0]
+  SQLStatement = 'SELECT * FROM user_recovery WHERE id = '+user.id+' AND recovery = "'+reqBody.recovery+'"'
+  var result = yield dbOps(SQLStatement,callback.next)
+  if(!result[0]) return return4Fail(401, 1, '恢复密钥错误。')
+  var token = authenticator.generateKey()
+  SQLStatement = 'UPDATE user SET token = "'+token+'" WHERE id = '+user.id
+  yield dbOps(SQLStatement,callback.next)
+  return return4Success({
+    qrcode: authenticator.generateTotpUri(token, (user.firstname).concat(user.lastname),'企业 OA 系统', 'SHA1', 6, 30)
   })
-}
+})}
